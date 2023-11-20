26c9508457fa95b153dd1bf242a39de4ff970c0f
github_christiandc19 <christiandc19@hotmail.com>
2023-11-20 15:44:37 -0800

    styling

--- src/components/SoberLiving/SoberLiving.js
+++ src/components/SoberLiving/SoberLiving.js
@@ -10,7 +10,7 @@ const SoberLiving = () => {
 
                 <div className="sober-living-content">
                     <Zoom duration={3000}>
-                        <h1>Check Out our Sober Living Homes</h1>
+                        <h1>Check Out our <span>Sober Living</span> Homes</h1>
                         <p>Welcome to our Sober Living Homes, a sanctuary where the journey to recovery continues with camaraderie, support, and the promise of a substance-free future.</p>
                     </Zoom>
                 </div>
