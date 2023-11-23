357fc22ea299764a32ddf405ddbc89595a967723
github_christiandc19 <christiandc19@hotmail.com>
2023-11-22 22:11:29 -0800

    styling

--- src/components/section4/Section4.jsx
+++ src/components/section4/Section4.jsx
@@ -4,13 +4,15 @@ import ImageSlider from './ImageSlider';
 import SoberLiving1 from "../../assets/north-citrus-bg.webp";
 import SoberLiving2 from "../../assets/sober-living-2.webp";
 import SoberLiving3 from "../../assets/sober-living-3.webp";
+import SoberLiving4 from "../../assets/sober-living.webp";
 
 
 const Section4 = () => {
   const images = [
     SoberLiving1,
     SoberLiving2,
-    SoberLiving3
+    SoberLiving3,
+    SoberLiving4
   ];
 
   return (
